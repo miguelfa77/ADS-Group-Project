2bdd3217807397806c1aad20f10c54822a6dfd36
miguelfa77 <100728907+miguelfa77@users.noreply.github.com>
2022-11-17 14:15:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
 
+#Introduction function introduces the user to the app.
+def Introduction():
+  print("Welcome to Noted - The marketplace for your notes at IE University, BBADBA, Year 2.")
+  #Asks the user if it wants to upload or find notes
+  print("\nDo you wish to upload your notes or find notes?")
