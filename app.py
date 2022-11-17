d651a4f684de0e085b3646d7502c33dfdcf14102
miguelfa77 <100728907+miguelfa77@users.noreply.github.com>
2022-11-17 14:25:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@
 def Introduction():
   print("Welcome to Noted - The marketplace for your notes at IE University, BBADBA, Year 2.")
   #Asks the user if it wants to upload or find notes
-  print("\nDo you wish to upload your notes or find notes?\nEnter 1 to upload notes:\nEnter 2 to find notes:\nEnter 3 to Quit:")
+  print("\nDo you wish to upload your notes or find notes?\nEnter U to upload notes:\nEnter F to find notes:\nEnter Q to Quit:")
