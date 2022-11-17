6c46cc2f8561a31abb3622ed7857cd7a9c4aa206
miguelfa77 <100728907+miguelfa77@users.noreply.github.com>
2022-11-17 15:03:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 
+
 #Introduction function introduces the user to the app.
 def Introduction():
   print("Welcome to Noted - The marketplace for your notes at IE University, BBADBA, Year 2.")
   #Asks the user if it wants to upload or find notes
   print("\nEnter U to upload notes:\nEnter F to find notes:\nEnter Q to Quit:")
+
+#notesFinder function takes availableNotes as input. 
+def notesFinder(availableNotes):
+    print("\nHere are the available courses: ")
+
+
+#notesUploader function to upload notes link.
+def notesUploader():
+
+#This function calls the rest of the functions
+def main():
+    Introduction()
+    
