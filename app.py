1cbbcda6417547204901b82dc581822e359411b0
PStoclet <118437030+PStoclet@users.noreply.github.com>
2022-11-17 14:19:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@
 def Introduction():
   print("Welcome to Noted - The marketplace for your notes at IE University, BBADBA, Year 2.")
   #Asks the user if it wants to upload or find notes
-  print("\nDo you wish to upload your notes or find notes?\nEnter 1 to upload notes\nEnter 2 to find notes")
+  print("\nDo you wish to upload your notes or find notes?\nEnter 1 to upload notes:\nEnter 2 to find notes:")
